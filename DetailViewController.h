73311fd2c3bef8dbc90d638d1b18e40c944233c0
199055 <1365160860@qq.com>
2016-08-30 16:42:51 +0800

    Add files via upload

---
+++ DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  BarrageTest
+//
+//  Created by zhangyapeng on 16/8/30.
+//  Copyright © 2016年 zhangyapeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+
+@end
+
---
+++ DetailViewController.m
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.m
+//  BarrageTest
+//
+//  Created by zhangyapeng on 16/8/30.
+//  Copyright © 2016年 zhangyapeng. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
